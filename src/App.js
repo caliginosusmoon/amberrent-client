35661911a05441106ed0cf454e7251dbbdadf9ee
Swastika Mohapatra <m2swastika@gmail.com>
2024-07-28 23:04:13 +0530

    about stories

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { BsPersonSquare } from "react-icons/bs";
 import logo from "./logo.svg";
 import Home from "./pages/home";
 import SignUp from "./pages/signup";
+import AboutPage from "./pages/about";
+import Stories from "./pages/stories";
 
 function App() {
     return (
         <div className="w-full max-width-[1300px]">
             {/* <Home/> */}
-            <SignUp />
+            {/* <SignUp /> */}
+            {/* <AboutPage/> */}
+            <Stories/>
         </div>
     );
 }
