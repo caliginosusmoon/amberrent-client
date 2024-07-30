2bb83b8d515ba9e4d479e8f174684d7acf4b65c8
caliginosusmoon <102291493+caliginosusmoon@users.noreply.github.com>
2024-07-30 23:14:12 +0530

    added fuctionality to login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
-import { BsPersonSquare } from "react-icons/bs";
-import logo from "./logo.svg";
-import Home from "./pages/home";
-=======
-<<<<<<< HEAD
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { BsPersonSquare } from "react-icons/bs";
 import logo from "./logo.svg";
 import Home from "./pages/home";
 import SignUp from "./pages/signup";
 import LogIn from "./pages/login";
-=======
-import { BsPersonSquare } from 'react-icons/bs';
-import logo from './logo.svg';
-import Home from './pages/home';
->>>>>>> main
->>>>>>> 49c5802df2ea1fb5ae1ab025e51c0de22a7d69ab
 
 function App() {
 	return (
 		<div className="w-full max-width-[1300px]">
-			<Home />
+			<BrowserRouter>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<LogIn />} />
+				<Route path="/signup" element={<sig />} />
+			</BrowserRouter>
 		</div>
 	);
 }
